d859702d6b4b908862a568e95385453155748f5f
millachen <30962470+millachen@users.noreply.github.com>
2021-05-17 15:46:45 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,9 +1,9 @@
 var vid;
 function setup() {
 /*canvas=createCanvas(100,100);*/
-canvas=createCanvas(1000,1000);
 vid=createVideo('video.mp4');
 vid.size(500,500);
+  vid.pos(0,0);
 vid.loop();
 //rect(10,10,10,10);
 }
