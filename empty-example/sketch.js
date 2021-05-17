810af89aec1afbdbd4caa88d0184f7b27cac6a21
millachen <30962470+millachen@users.noreply.github.com>
2021-05-17 15:29:14 +0200

    Update sketch.js

--- empty-example/sketch.js
+++ empty-example/sketch.js
@@ -1,10 +1,10 @@
 var vid;
 function setup() {
-canvas=createCanvas(100,100);
+/*canvas=createCanvas(100,100);*/
+canvas=createCanvas(1000,1000);
 rect(10,10,10,10);
-/*canvas=createCanvas(1000,1000);
 vid=createVideo('video.mp4');
-vid.size(500,500);*/
+vid.size(500,500);
  
 }
 
